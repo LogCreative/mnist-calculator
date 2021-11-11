62533bf3aa4ee02c03b922f6e1dd626eb6a8bb95
LogCreative <lizilong123456@live.cn>
2021-11-11 11:02:03 +0800

    task3 draw lines

--- task3/ui.py
+++ task3/ui.py
@@ -1,14 +1,42 @@
-import tkinter as tk
-root = tk.Tk()
+from tkinter import *
+root = Tk()
 
-w = tk.Canvas(root, width=400, height=200)
-w.pack()
+class WriteArea(Canvas):
+    def __init__(self, master, **kwargs):
+        Canvas.__init__(self, master, kwargs)
+        self.bind("<Button-1>", self.mouse_down)
+        self.bind("<B1-Motion>", self.paint)
+        self.bind("<B1-ButtonRelease>", self.mouse_up)
+        self.startPoint = 0, 0
 
-def paint(event):
-    x1, y1 = (event.x - 1), (event.y - 1)
-    x2, y2 = (event.x + 1), (event.y + 1)
-    w.create_oval(x1, y1, x2, y2, fill="black")
+    def mouse_down(self, event):
+        self.startPoint = event.x, event.y
 
-w.bind("<B1-Motion>", paint)
+    def paint(self, event):
+        self.create_line(self.startPoint,
+                         event.x, 
+                         event.y, 
+                         joinstyle="round", 
+                         width=3,
+                         fill="black")
+        self.startPoint = event.x, event.y
+
+    def mouse_up(self, event):
+        print("Mouse released at ", event.x, event.y)
+
+c = WriteArea(root, width=600, height=200, bg="white")
+c.pack()
+
+def clean():
+    print("Will be cleaned.")
+
+b = Button(root, width=20, height=5, text="Erase", command=clean)
+b.pack()
+
+output_text = Label(root, width=40, height=5)
+output_text.pack()
+
+s = Checkbutton(root, width=20, height=5, text="Visualize Stroke Groups")
+s.pack()
 
 root.mainloop()
