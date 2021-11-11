87e5996c6c7d59c076b91686fa5ec63e992cb86c
LogCreative <lizilong123456@live.cn>
2021-11-11 20:59:51 +0800

    task3 ui framework

--- task3/ui.py
+++ task3/ui.py
@@ -1,13 +1,13 @@
 from tkinter import *
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 
 root = Tk()
 
 def getter(widget):
-    x=root.winfo_rootx()+widget.winfo_x()
-    y=root.winfo_rooty()+widget.winfo_y()
-    x1=x+widget.winfo_width()
-    y1=y+widget.winfo_height()
+    x = root.winfo_rootx() + widget.winfo_x()
+    y = root.winfo_rooty() + widget.winfo_y()
+    x1 = x + widget.winfo_width()
+    y1 = y + widget.winfo_height()
     return ImageGrab.grab().crop((x,y,x1,y1))
 
 class WriteArea(Canvas):
@@ -17,40 +17,63 @@ class WriteArea(Canvas):
         self.bind("<B1-Motion>", self.paint)
         self.bind("<B1-ButtonRelease>", self.mouse_up)
         self.startPoint = 0, 0
+        self.strokePoints = []
+        self.allStrokes = []
 
     def mouse_down(self, event):
         self.startPoint = event.x, event.y
+        self.strokePoints.clear()
 
     def paint(self, event):
-        self.create_line(self.startPoint,
+        self.create_line(self.startPoint[0],
+                         self.startPoint[1],
                          event.x, 
                          event.y, 
                          joinstyle="round", 
+                         capstyle="round",
                          width=5,
                          fill="black",
                          tags="inputs")
-        self.startPoint = event.x, event.y
+        pos = tuple([event.x, event.y])
+        self.startPoint = pos
+        self.strokePoints.append(pos)
 
     def mouse_up(self, event):
-        print("Mouse released at ", event.x, event.y)
-        im = getter(self)
+        self.allStrokes.append(self.strokePoints)
+        self.visualize()
+        output_text.set("Prediction")
 
     def clean(self):
         self.delete("inputs")
 
+    def visualize(self):
+        if visual.get() == "yes": 
+            print("I will visualize")
+        else: 
+            print("I will recover")
+
 c = WriteArea(root, width=600, height=200, bg="white")
 c.pack()
 
+output_text = StringVar()
+
 def clean():
     c.clean()
+    output_text.set("")
 
 b = Button(root, width=20, height=5, text="Erase", command=clean)
 b.pack()
 
-output_text = Label(root, width=40, height=5)
-output_text.pack()
+r = Label(root, width=40, height=5, textvariable=output_text)
+r.pack()
 
-s = Checkbutton(root, width=20, height=5, text="Visualize Stroke Groups")
+visual = StringVar()
+visual.set("no")
+
+def visualize():
+    c.visualize()
+
+s = Checkbutton(root, width=20, height=5, variable=visual, text="Visualize Stroke Groups", onvalue="yes", offvalue="no", command=visualize)
 s.pack()
 
 root.mainloop()
