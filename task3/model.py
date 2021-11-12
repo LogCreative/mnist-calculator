a77515fdef51dc98700e57f783f88e715ca9b76f
LogCreative <lizilong123456@live.cn>
2021-11-12 13:54:32 +0800

    task3 failed PIL screenshot

--- task3/model.py
+++ task3/model.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
+import numpy as np
 
 # https://blog.csdn.net/thriving_fcl/article/details/75213361
 sess = tf.Session(graph=tf.Graph())
-meta_graph = tf.saved_model.loader.load(sess,[tf.saved_model.tag_constants.SERVING],'export_ext')
+meta_graph = tf.saved_model.loader.load(sess,[tf.saved_model.tag_constants.SERVING],'../export_ext')
 
 input = sess.graph.get_tensor_by_name(
     meta_graph.signature_def['Infer'].inputs["inputs"].name)
@@ -11,4 +12,8 @@ infer_method = sess.graph.get_tensor_by_name(
     meta_graph.signature_def['Infer'].outputs["outputs"].name)
 
 def infer(_input):
+    img = np.array(_input)
+    img = 0.5 - img
+    print(img)
+    return 0
     return sess.run(infer_method, feed_dict={input:_input})
--- task3/ui.py
+++ task3/ui.py
@@ -1,16 +1,25 @@
 from tkinter import *
 from PIL import ImageGrab, Image
+from tensorflow.python.ops.gen_math_ops import exp
 from colors import getColor
 from model import infer
 
 root = Tk()
 
-def getter(widget):
-    x = root.winfo_rootx() + widget.winfo_x()
-    y = root.winfo_rooty() + widget.winfo_y()
-    x1 = x + widget.winfo_width()
-    y1 = y + widget.winfo_height()
-    return ImageGrab.grab().crop((x,y,x1,y1))
+def grabArea(widget, lt, rb):
+    x = root.winfo_rootx() + widget.winfo_x() + lt[0]
+    y = root.winfo_rooty() + widget.winfo_y() + lt[1]
+    x1 = root.winfo_rootx() + widget.winfo_x() + rb[0]
+    y1 = root.winfo_rooty() + widget.winfo_y() + rb[1]
+    img = ImageGrab.grab().crop((x,y,x1,y1))
+    w = rb[0] - lt[0]
+    h = rb[1] - lt[1]
+    a = w if w > h else h
+    exp_img = Image.new('F',(a,a),0)
+    exp_img.paste(img, ((a-w)//2,(a-h)//2))
+    exp_img = exp_img.resize((28,28))
+    exp_img.show()
+    return img
 
 class WriteArea(Canvas):
     def __init__(self, master, **kwargs):
@@ -36,12 +45,23 @@ class WriteArea(Canvas):
         self.strokePoints.append(pos)
 
     def mouse_up(self, event):
-        self.allStrokes.append(self.strokePoints.copy())
-        self.visualize()
+        if len(self.strokePoints) > 0:
+            self.allStrokes.append(self.strokePoints.copy())
         groupDict = self.grouping()
-        # Generate group graph
-        
-        output_text.set("Prediction")
+        for gid in groupDict.keys():
+            self.delete("inputs")
+            # the bounding box of this group
+            gl = groupDict[gid][1]
+            gr = groupDict[gid][2]
+            gt = 1000
+            gb = 0
+            for stroke_id in groupDict[gid][0]:
+                self.draw_stroke(self.allStrokes[stroke_id])
+                gt = min(min(p[1] for p in self.allStrokes[stroke_id]), gt)
+                gb = max(max(p[1] for p in self.allStrokes[stroke_id]), gb)
+            result = infer(grabArea(c, [gl,gt], [gr,gb]))
+            print(result)
+        self.visualize()
 
     def clean(self):
         self.delete("inputs")
@@ -54,6 +74,12 @@ class WriteArea(Canvas):
             prev = stroke[_]
     
     def grouping(self):
+        """
+        return a groupDict:
+        [0]: the id's belong to this group
+        [1]: the leftend of this group
+        [2]: the rightend of this group
+        """
         self.allStrokes.sort(key=lambda stroke: min([p[0] for p in stroke]))
         groupDict = {}
         ggid = 0
@@ -65,7 +91,11 @@ class WriteArea(Canvas):
             for gid in groupDict.keys():
                 if rightend <= groupDict[gid][2] or \
                     (groupDict[gid][2] - leftend) / (groupDict[gid][2] - groupDict[gid][1]) > 0.1:
-                    groupDict[gid][0].append(_)     # contains or mostly overlapped
+                    # contains or mostly overlapped
+                    groupDict[gid][0].append(_)
+                    # update the edge point
+                    groupDict[gid][1] = min(groupDict[gid][1], leftend)
+                    groupDict[gid][2] = max(groupDict[gid][2], rightend)
                     flag = True
                     break
             if flag: continue
